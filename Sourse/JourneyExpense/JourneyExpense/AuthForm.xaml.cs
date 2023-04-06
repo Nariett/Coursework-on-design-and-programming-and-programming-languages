691387cb37cbf2f78fb3df4e786782ea3f97b479
Sasha Samoilov <samsiki50@gmail.com>
2023-04-06 21:21:36 +0300

    Add new app

---
+++ Sourse/JourneyExpense/JourneyExpense/AuthForm.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JourneyExpense
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthForm.xaml
+    /// </summary>
+    public partial class AuthForm : Window
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы прошли аутентификаицю");
+            MainWindow MainForm= new MainWindow();
+            MainForm.Show();
+        }
+
+        private void RegButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
